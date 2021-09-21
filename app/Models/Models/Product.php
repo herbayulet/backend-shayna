71a770b5041de897947c4bf3d97a7b2c06ea4fce
herbayulet <sayaherbayu@gmail.com>
2021-09-21 13:46:07 +0700

    Learn BE Shayna

---
+++ app/Models/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name', 'slug', 'type', 'description', 'price', 'quantity'
+    ] ;
+
+    protected $hidden = [
+
+    ];
+
+    public function galleries () {
+        
+    }
+}
